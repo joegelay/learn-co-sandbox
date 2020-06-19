38d98761fdb8386a8b52ae456698deab27bbf292
Joe Gelay <joegelay@gmail.com>
2020-06-19 22:25:28 +0000

    Automatically backed up by Learn

---
+++ learning-comments.rb
@@ -0,0 +1,10 @@
+puts "Joy to the world"
+puts "All the boys and girls"
+# puts "Joy to the fishes in the deep blue sea"
+puts "Joy to you and me"
+
+=begin
+this is another
+way to create a multi-line comment
+=end
+
